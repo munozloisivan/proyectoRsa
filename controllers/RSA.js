cc9e29675a4b5cc3767204de7c807d5e4d0463d5
Roberto <roberto.arranz.93@gmail.com>
2017-10-22 17:41:52 +0200

    optimizacion de código y update visual

--- controllers/RSA.js
+++ controllers/RSA.js
@@ -1,33 +1,9 @@
 var bigInt = require("big-integer");
-
 //Generamos todos los valores cuando se arranca el servidor
 var length = 512;
+var p, q, n, phiN, e, d;
 
-var p = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
-var q = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
-
-while (!bigInt(p).isPrime()){
-    p = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
-}
-
-while (!bigInt(q).isPrime()){
-    q = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
-}
-
-//n = p·q y phiN es (p-1)(q-1)
-var n = bigInt(p).multiply(q);
-var phiN = bigInt(bigInt(p).prev()).multiply(bigInt(q).prev());
-
-//Random menor que Phi de N
-var e = bigInt.randBetween("1e90", bigInt(phiN));
-
-//Mientras e no sea primo y coprimo de phiN lo volvemos a generar
-while ((!bigInt(e).isPrime())&&(bigInt.lcm(e, phiN) != 1)){
-    e = bigInt.randBetween("1e90", bigInt(phiN));
-}
-
-//d (exponente privado) tiene que ser el multiplicador inverso de e mod phiN
-var d = bigInt(e).modInv(phiN);
+generateKeys();
 
 //GET - Return Public Key
 exports.publicKey = function(req, res) {
@@ -38,32 +14,7 @@ exports.publicKey = function(req, res) {
 //Get - Reset Keys
 exports.redoKey = function(req, res) {
     console.log('GET /redoKey');
-
-    p = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
-    q = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
-
-    while (!bigInt(p).isPrime()){
-        p = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
-    }
-
-    while (!bigInt(q).isPrime()){
-        q = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
-    }
-
-//n = p·q y phiN es (p-1)(q-1)
-    n = bigInt(p).multiply(q);
-    phiN = bigInt(bigInt(p).prev()).multiply(bigInt(q).prev());
-
-//Random menor que Phi de N
-    e = bigInt.randBetween("1e90", bigInt(phiN));
-
-//Mientras e no sea primo y coprimo de phiN lo volvemos a generar
-    while ((!bigInt(e).isPrime())&&(bigInt.lcm(e, phiN) != 1)){
-        e = bigInt.randBetween("1e90", bigInt(phiN));
-    }
-
-//d (exponente privado) tiene que ser el multiplicador inverso de e mod phiN
-    d = bigInt(e).modInv(phiN);
+    generateKeys();
     res.status(200).jsonp("Las claves se han generado de nuevo");
 };
 
@@ -89,28 +40,31 @@ function hex_to_ascii(str1)
     }
     return str;
 }
-/*
-//PUT - Update a register already exists
-exports.updateStudent = function(req, res) {
-    Student.findById(req.params.id, function(err, student) {
-        student.nombre = req.body.nombre;
-        student.apellido = req.body.apellido;
-        student.edad = req.body.edad;
-        student.genero  = req.body.genero;
 
-        student.save(function(err) {
-            if(err) return res.status(500).send(err.message);
-            res.status(200).jsonp(student);
-        });
-    });
-};
+function  generateKeys() {
+    p = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
+    q = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
 
-//DELETE - Delete a Student specified ID
-exports.deleteStudent = function(req, res) {
-    Student.findById(req.params.id, function(err, student) {
-        student.remove(function(err) {
-            if(err) return res.status(500).send(err.message);
-            res.status(200).send();
-        })
-    });
-};*/
+    while (!bigInt(p).isPrime()){
+        p = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
+    }
+
+    while (!bigInt(q).isPrime()){
+        q = bigInt.randBetween(bigInt(2).pow((length/2) - 1), bigInt(bigInt(2).pow(length/2)).minus(1));
+    }
+
+//n = p·q y phiN es (p-1)(q-1)
+    n = bigInt(p).multiply(q);
+    phiN = bigInt(bigInt(p).prev()).multiply(bigInt(q).prev());
+
+//Random menor que Phi de N
+    e = bigInt.randBetween((bigInt(2).pow(length/2)), bigInt(phiN));
+
+//Mientras e no sea primo y coprimo de phiN lo volvemos a generar
+    while ((!bigInt(e).isPrime())&&(bigInt.lcm(e, phiN) != 1)){
+        e = bigInt.randBetween((bigInt(2).pow(length/2)), bigInt(phiN));
+    }
+
+//d (exponente privado) tiene que ser el multiplicador inverso de e mod phiN
+    d = bigInt(e).modInv(phiN);
+}
